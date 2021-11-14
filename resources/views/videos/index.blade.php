6df27fbf0b4e2d7cf3a6c09f5682da2e4dc28a4b
malorbc <malo.robic@gmail.com>
2021-11-14 11:59:37 +0100

    style videos

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -1,22 +1,43 @@
 <x-app-layout>
+    <style>
+        .container-videos {
+            position: relative;
+            overflow: hidden;
+            width: 100%;
+            padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+        }
+
+        .iframe-videos {
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-center text-gray-800 leading-tight">
             {{ __('Video') }}
         </h2>
     </x-slot>
-    <div class="flex flex-col justify-center items-center bg-white">
+    <div class="flex flex-row  flex-wrap justify-center items-center p-4 bg-white rounded-lg w-full sm:w-3/4 mb-4 m-auto mt-4 shadow">
         @foreach($videos as $video)
         @php
         $videoURL = $video->url;
         $newVideoURL = str_replace('watch?v=', 'embed/', $videoURL)
         @endphp
-        <div class="aspect-w-16 aspect-h-9 m-4">
-            <iframe width="560" height="315" src="{{$newVideoURL}}" title="{{$video->title}}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="w-1/3 p-4">
+            <div class="w-full container-videos">
+                <iframe class="iframe-videos" src="{{$newVideoURL}}" title="{{$video->title}}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
         </div>
          @endforeach  
     </div>
-    <a href="{{route('videos.create')}}">
-    <x-button class="bg-indigo-500 hover:bg-indigo-300 rounded-none" style="margin:10px;">Ajouter une video</x-button>
-    </a>
-    
+    <div class="bouton w-full sm:w-3/4 m-auto">
+        <a href="{{route('videos.create')}}">
+        <x-button class="bg-indigo-500 hover:bg-indigo-300 rounded-none">Ajouter une video</x-button>
+        </a> 
+    </div>
 </x-app-layout>
