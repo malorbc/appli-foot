a56ab3a8793fa5472da1c2e811f0077023994b38
malorbc <malo.robic@gmail.com>
2021-12-01 15:23:48 +0100

    debut landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,26 +4,26 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Bienvenue sur appli foot</title>
+        <title>Bienvenue sur myBall</title>
         <script src="{{ asset('js/test-chart.js')}}" type="module"></script>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}}
-        </style>
 
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+         <!-- Styles -->
+         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex flex-col min-h-screen bg-gray-100 py-4 sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="w-screen px-6 py-4 border-b-2 border-indigo-500 flex justify-end">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Mon espace</a>
                     @else
@@ -35,9 +35,52 @@
                     @endauth
                 </div>
             @endif
-
-            <div>
-                <h1>Appli foot</h1>
+            <div class="p-4">
+                <h1 class="text-center text-3xl mt-12 m-auto">Découvrez <span class="text-indigo-500 font-bold">myBall</span></h1>
+            </div>
+            <div class="w-2/3 mx-auto mt-4 mb-4">
+                <div class="bg-white shadow-sm rounded-lg flex flex-row">
+                    <div class="w-1/2">
+                        <img src="{{url('/images/image14.jpg')}}" alt="" srcset="">
+                    </div>
+                    <div class="w-1/2 p-4">
+                        <h2 class="text-xl mb-2">Une application utilisée par les <span class='text-green-500'>professionnels</span></h2>
+                        <p>Créée par et pour les joueurs, myBall vous aidera au quotidien pour gérer votre équipe. Inscrivez vous gratuitement pour commencer à utiliser l'application.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-2/3 mx-auto mt-4 mb-4">
+                <div class="bg-white shadow-sm rounded-lg flex flex-row-reverse">
+                    <div class="w-1/2">
+                        <img src="{{url('/images/image14.jpg')}}" alt="" srcset="">
+                    </div>
+                    <div class="w-1/2 p-4">
+                        <h2 class="text-xl mb-2">Une application utilisée par les <span class='text-green-500'>professionnels</span></h2>
+                        <p>Créée par et pour les joueurs, myBall vous aidera au quotidien pour gérer votre équipe. Inscrivez vous gratuitement pour commencer à utiliser l'application.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-2/3 mx-auto mt-4 mb-4">
+                <div class="bg-white shadow-sm rounded-lg flex flex-row">
+                    <div class='w-1/3 p-4 flex flex-col justify-items-center items-center text-center'>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-indigo-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+                          </svg>
+                          <p>Une gestion simplifiée de vos évènements</p>
+                    </div>
+                    <div class='w-1/3 p-4 flex flex-col justify-items-center items-center text-center'>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-indigo-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                          </svg>
+                          <p>Vos replay de matchs, accessibles en 1 clic</p>
+                    </div>
+                    <div class='w-1/3 p-4 flex flex-col justify-items-center items-center text-center'>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-indigo-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
+                          </svg>
+                          <p>Toutes vos statisitques au même endroit</p>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
