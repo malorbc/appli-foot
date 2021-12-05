c65413a50edf636a83ebd679968687157a77256b
malorbc <malo.robic@gmail.com>
2021-12-05 18:50:01 +0100

    name et favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Appli foot') }}</title>
+        <title>{{ config('app.name', 'Myball') }}</title>
         {{-- <title>Foot</title> --}}
 
         <!-- Fonts -->
